65541a987fdfe4cdf9558c9f1a789fa25b262d96
Mamoudou Sow <sowmkb@yahoo.com>
2018-04-23 17:28:56 -0400

    check

--- 5.Projet/300102828/app/controllers/RecieverController.scala
+++ 5.Projet/300102828/app/controllers/RecieverController.scala
@@ -23,7 +23,7 @@ POST    /receiver        controllers.ReceiverController.receiverPost
 /**
   * User form controller for Play Scala
   */
-class RecieverController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider,
+class ReceiverController @Inject()(protected val dbConfigProvider: DatabaseConfigProvider,
                                    mcc: MessagesControllerComponents
                                   )(implicit ec: ExecutionContext)
   extends MessagesAbstractController(mcc) with HasDatabaseConfigProvider[JdbcProfile] {
@@ -34,32 +34,33 @@ class RecieverController @Inject()(protected val dbConfigProvider: DatabaseConfi
       "lastName" -> text,
       "firstName" -> text,
       "tel" -> text,
-    )(Reciever.apply)(Reciever.unapply)
+    )(Receiver.apply)(Receiver.unapply)
   )
 
-  def recieverSearch(name: String) = Action.async { implicit request =>
-    val resultingRecievers: Future[Seq[Reciever]] = db.run(recievers.filter(_.lastName === name).result)
-    resultingRecievers.map(x =>Ok(views.html.reciever.list(x)))
+  def receiverSearch(name: String) = Action.async { implicit request =>
+    val resultingReceivers: Future[Seq[Receiver]] = db.run(receivers.filter(_.lastName === name).result)
+    resultingReceivers.map(x =>Ok(views.html.receiver.list(x)))
   }
 
   def receiverGet() = Action { implicit request: MessagesRequest[AnyContent] =>
-    Ok(views.html.reciever.form(recieverForm))
+    Ok(views.html.receiver.form(receiverForm))
   }
 
   def receiverPost() = Action { implicit request: MessagesRequest[AnyContent] =>
     receiverForm.bindFromRequest.fold(
       formWithErrors => {
         // binding failure, you retrieve the form containing errors:
-        BadRequest(views.html.reciever.form(formWithErrors))
+        BadRequest(views.html.receiver.form(formWithErrors))
       },
-      recieverData => {
+      receiverData => {
         /* binding success, you get the actual value. */
         /* flashing uses a short lived cookie */
-        val recieverId = (recievers returning recievers.map(_.id)) += recieverData
-        db.run(recieverId)
-        Redirect(routes.ReceiverController.recieverGet()).flashing("success" -> ("Successful " + recieverData.toString))
+        val receiverId = (receivers returning receivers.map(_.id)) += receiverData
+        db.run(receiverId)
+        Redirect(routes.ReceiverController.receiverGet()).flashing("success" -> ("Successful " + receiverData.toString))
       }
     )
   }
 }
+
 
--- 5.Projet/300102828/app/models/Recievers.scala
+++ 5.Projet/300102828/app/models/Recievers.scala
@@ -3,7 +3,7 @@ package models
 import slick.jdbc.MySQLProfile.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case class  Reciever(id: Int
+case class  Receiver(id: Int
                      ,lastName: String
                      ,firstName: String
                      ,tel: String
@@ -11,7 +11,7 @@ case class  Reciever(id: Int
 
 
 // Definition of the SUPPLIERS table
-class Recievers(tag: Tag) extends Table[Reciever](tag, "Receiver") {
+class Receivers(tag: Tag) extends Table[Receiver](tag, "Receiver") {
   def id = column[Int]("idReceiver", O.PrimaryKey,O.AutoInc) // This is the primary key column
   def lastName = column[String]("LastName")
   def firstName = column[String]("FirstName")
@@ -19,5 +19,5 @@ class Recievers(tag: Tag) extends Table[Reciever](tag, "Receiver") {
 
 
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = (id, lastName, firstName,tel) <> (Reciever.tupled, Reciever.unapply)
+  def * = (id, lastName, firstName,tel) <> (Receiver.tupled, Receiver.unapply)
 }
